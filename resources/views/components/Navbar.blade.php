0c5c329a3742144999a41253958f2331b625fa2c
Jorge Cortés <georgeluis.idem@gmail.com>
2020-07-30 01:14:47 -0500

    Avances de Header y Slider

--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -1,21 +1,43 @@
 @section('components.Navbar')
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="index.html"><span>Still</span> <span>Hope México</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="oi oi-menu"></span> Men&uacute;
-            </button>
-
-            <div class="collapse navbar-collapse" id="ftco-nav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="index.html" class="nav-link">Inicio</a></li>
-                    <li class="nav-item"><a href="about.html" class="nav-link">Nosotros</a></li>
-                    <li class="nav-item"><a href="events.html" class="nav-link">Eventos</a></li>
-                    <li class="nav-item"><a href="sermons.html" class="nav-link">Tienda</a></li>
-                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact.html" class="nav-link">Contacto</a></li>
-                </ul>
+    <nav class="navbar navbar-expand-lg fixed-top top">
+        <div class="d-flex justify-content-between" style="width: 100%">
+            <span class="cintillo-text">Tenemos más de 149,000 razones para decirte Si</span>
+            <span class="cintillo-text">Contactanos: (999) 912 01 00\ ventas@maderorefaccionarias.com.mx</span>
+            <div>
+                <span class="cintillo-text text-white">Siguenos en </span>
+                <img class="cintillo-social-icons" src="/images/cintillo-superior-fijo/02-icono-whatsapp.png" />
+                <img class="cintillo-social-icons" src="/images/cintillo-superior-fijo/03-icono-facebook.png" />
+                <img class="cintillo-social-icons" src="/images/cintillo-superior-fijo/04-icono-instagram.png" />
             </div>
         </div>
     </nav>
+
+    <div class="nav-scroller encabezado">
+        <nav class="nav">
+            <div class="d-flex justify-content-between" style="width: 100%">
+                <img class="encabezado-logo" src="/images/encabezado/05-logo-madero.png" />
+                <img class="encabezado-slogan" src="/images/encabezado/06-slogan-blanco.png" />
+                <div class="encabezado-icons-container">
+                    <img class="encabezado-icons" src="/images/encabezado/07-icono-login.png" />
+                    <img class="encabezado-icons" src="/images/encabezado/08-icono-wishlist.png" />
+                    <img class="encabezado-icons" src="/images/encabezado/09-icono-checkout.png" />
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="menu">
+        <div class="menu-items d-flex justify-content-center">
+            <ul class="list-inline">
+                <li class="list-inline-item">INICIO</li>
+                <li class="list-inline-item">CONÓCENOS</li>
+                <li class="list-inline-item">PRODUCTOS</li>
+                <li class="list-inline-item">PROMOCIONES</li>
+                <li class="list-inline-item">EXPERT TIPS</li>
+                <li class="list-inline-item">UNETE A LA FAMILIA</li>
+                <li class="list-inline-item">ATENCIÓN AL CLIENTE</li>
+                <li class="list-inline-item">CONTÁCTANOS</li>
+            </ul>
+        </div>
+    </div>
 @endsection
--- resources/views/components/Scripts.blade.php
+++ resources/views/components/Scripts.blade.php
@@ -1,21 +1,7 @@
 @section('components.Scripts')
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
-    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
-    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('js/aos.js') }}"></script>
-    <script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
-    <script src="{{ asset('js/jquery.mb.YTPlayer.min.js') }}"></script>
-    <script src="{{ asset('js/scrollax.min.js') }}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="{{ asset('js/google-map.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
 
     @yield('scripts')
 @endsection
