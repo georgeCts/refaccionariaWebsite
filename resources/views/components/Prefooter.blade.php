0ebf77e5048da0051c0a62685c6888e572aec425
Jorge Cortés <georgeluis.idem@gmail.com>
2021-05-03 19:18:04 -0500

    Correcciones menores 3 Mayo

--- resources/views/components/Prefooter.blade.php
+++ resources/views/components/Prefooter.blade.php
@@ -2,6 +2,10 @@
     <div class="caracteristicas">
         <div class="container">
             <div class="row text-center">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="wrapper"><img src="/images/caracteristicas/ubicacion.png" alt=""></div>
+                    <p>Con más de 40 años <br> en el mercado</p>
+                </div>
                 <div class="col-12 col-md-6 col-lg-3 m20 mb-lg-0">
                     <div class="wrapper"><img src="/images/caracteristicas/delivery.png" alt="" class="ml-n3"></div>
                     <p>Llegamos a toda la <br> República Mexicana</p>
@@ -13,10 +17,6 @@
                 <div class="col-12 col-md-6 col-lg-3 m20 mb-lg-0">
                     <div class="wrapper"><img src="/images/caracteristicas/mesa_trabajo.png" alt=""></div>
                     <p>Catálogos en <br> línea, más rápido</p>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="wrapper"><img src="/images/caracteristicas/ubicacion.png" alt=""></div>
-                    <p>Con más de 40 años <br> en la industria</p>
                 </div>
             </div>
         </div>
